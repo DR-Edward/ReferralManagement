f2403d239ea35bf002063faa534a5eda32bd86df
DR-Edward <reyesiovanny@gmail.com>
2025-10-17 03:12:59 -0600

    adds admin menu, submenu and basic configuration page

---
+++ ReferralManagement/Controller/Adminhtml/Mistery/Index.php
@@ -0,0 +1,34 @@
+<?php
+namespace WolfSellers\ReferralManagement\Controller\Adminhtml\Mistery;
+
+use Magento\Backend\App\Action;
+use Magento\Framework\View\Result\PageFactory;
+
+class Index extends Action
+{
+    const ADMIN_RESOURCE = 'WolfSellers_ReferralManagement::management';
+
+    /**
+     * @var PageFactory
+     */
+    protected $resultPageFactory;
+
+    public function __construct(
+        Action\Context $context,
+        PageFactory $resultPageFactory
+    ) {
+        parent::__construct($context);
+        $this->resultPageFactory = $resultPageFactory;
+    }
+
+    /**
+     * Execute method
+     */
+    public function execute()
+    {
+        $resultPage = $this->resultPageFactory->create();
+        $resultPage->setActiveMenu('WolfSellers_ReferralManagement::management');
+        $resultPage->getConfig()->getTitle()->prepend(__('Aquí pondría una nueva customización si me contratan 😏'));
+        return $resultPage;
+    }
+}
